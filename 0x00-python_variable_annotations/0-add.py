e9c1cbf01f5c7fbe512ebbb1a7b08b02f41931e8
realjioke <scarlobest@gmail.com>
2023-12-08 19:51:13 +0000

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This module uses type annotation to declare typed function """
+
+
+def add(a: float, b: float) -> float:
+    """ Typed add function using typedinputs and output """
+
+    return a + b
